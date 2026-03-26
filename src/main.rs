2ffa9652c4abbadd8d84c9bcd07c8d64bea2017f
Niket Naidu <coder137@gmail.com>
2026-03-25 23:11:47 -0700

    Update copyright footer text

--- src/main.rs
+++ src/main.rs
@@ -106,7 +106,7 @@ pub fn WebsiteFooter() -> Element {
         daisyui::Footer {
             class: "footer-horizontal bg-base-200 rounded-field flex justify-between items-center p-2",
             center: false,
-            p { class: "text-base", "\u{00A9} 2025 Niket Naidu. All rights reserved." }
+            p { class: "text-base", "\u{00A9} 2026 Niket Naidu. All rights reserved." }
             WebsiteSocials {}
         }
     }
